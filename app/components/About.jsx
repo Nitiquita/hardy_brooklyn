96c47eb0e14bf04ffc592fa0cd2c4a52ec5dd459
Nitika Nadgar <nitiquita@gmail.com>
2018-03-05 08:39:08 -0500

    adding responsive css"

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -26,33 +26,39 @@ export default class About extends Component {
             tech company, worked in kosher foods, worked in event planning and
             large scale festivals and started a business development consultant
             company. Yet throughout his successes, he still felt like there was
-            something still missing, something larger…
+            something still missing,
+            <br />
+            something larger…
           </div>
         </div>
         <div className="circle">
           <div id="circle-2">
-            One day, Hardy
+            <p>One day, Hardy
             <br />
             and a friend walked into a New Year’s Day party and he was chosen to
-            become a body painting model. With his timeless good looks and
+            become a body painting model.
+            <br/>
+            With his timeless good looks and
             industry standard physique, Hardy Brooklyn was a perfect specimen
-            for both body painters and photographers alike.
+            for both body painters
+            <br />
+            and photographers alike.</p>
           </div>
         </div>
         <div className="circle">
           <div id="circle-3">
-            Within two years,
+            <p>Within two years,
             <br />
             he made a name for himself in the underground arts, music and
             fashion scene. He is an instant hit at parties and his energy is
-            synonymous with being the life of the party.
+            synonymous with being the life of the party.</p>
           </div>
         </div>
         <div className="circle">
           <div id="circle-4">
-            Hardy is inherently and fundamentally a performance artist. He hopes
+            <p>Hardy is inherently and fundamentally a performance artist. He hopes
             to propel the interactive themed costume art scene and is open to
-            new connections, interactive events and photo ops.
+            new connections, interactive events and photo ops.</p>
           </div>
         </div>
       </section>
